4e7f9d0367184d46dce2af77d4b8570d56f1b888
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-05-23 11:49:16 -0400

    Worked on schema.sql

---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Full Stack Developer', 85000, 1),
+('Software Engineer', 125000, 1),
+('Accountant', 60000, 2),
+('Financial Analyst', 155000, 2),
+('Marketing Coordinator', 75000, 3),
+('Sales Lead', 95000, 3),
